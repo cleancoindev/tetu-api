e5a11ee187a631f949e9b491495eeb67658f07e1
belbix <7453635@gmail.com>
2021-09-05 14:52:42 +0300

    remove ts-mocha and sorting our dependency

--- src/services/vault-service.ts
+++ src/services/vault-service.ts
@@ -1,6 +1,6 @@
 import {BigNumber, ethers} from "ethers";
-import {SmartVault, SmartVault__factory} from "../typechain";
-import {TetuApi} from "./tetu-api";
+import {SmartVault, SmartVault__factory} from "../../typechain";
+import {TetuApi} from "../tetu-api";
 import {HttpProvider} from "web3-core";
 
 export class VaultService {
@@ -19,35 +19,35 @@ export class VaultService {
   }
 
   public async getPeriodFinishForToken(address: string, token: string): Promise<BigNumber> {
-    return this.tetuApi.web3Call1(this.instance(address).periodFinishForToken, token, address + ' periodFinishForToken');
+    return this.tetuApi.web3Call1(this.instance(address).periodFinishForToken, token, address + " periodFinishForToken");
   }
 
   public async getRewardRateForToken(address: string, token: string): Promise<BigNumber> {
-    return this.tetuApi.web3Call1(this.instance(address).rewardRateForToken, token, address + ' rewardRateForToken');
+    return this.tetuApi.web3Call1(this.instance(address).rewardRateForToken, token, address + " rewardRateForToken");
   }
 
   public async getLastUpdateTimeForToken(address: string, token: string): Promise<BigNumber> {
-    return this.tetuApi.web3Call1(this.instance(address).lastUpdateTimeForToken, token, address + ' lastUpdateTimeForToken');
+    return this.tetuApi.web3Call1(this.instance(address).lastUpdateTimeForToken, token, address + " lastUpdateTimeForToken");
   }
 
   public async getRewardPerTokenStoredForToken(address: string, token: string): Promise<BigNumber> {
-    return this.tetuApi.web3Call1(this.instance(address).rewardPerTokenStoredForToken, token, address + ' rewardPerTokenStoredForToken');
+    return this.tetuApi.web3Call1(this.instance(address).rewardPerTokenStoredForToken, token, address + " rewardPerTokenStoredForToken");
   }
 
   public async getUserRewardPerTokenPaidForToken(address: string, token: string, user: string): Promise<BigNumber> {
-    return this.tetuApi.web3Call2(this.instance(address).userRewardPerTokenPaidForToken, token, user, address + ' userRewardPerTokenPaidForToken');
+    return this.tetuApi.web3Call2(this.instance(address).userRewardPerTokenPaidForToken, token, user, address + " userRewardPerTokenPaidForToken");
   }
 
   public async getRewardsForToken(address: string, token: string, user: string): Promise<BigNumber> {
-    return this.tetuApi.web3Call2(this.instance(address).rewardsForToken, token, user, address + ' rewardsForToken');
+    return this.tetuApi.web3Call2(this.instance(address).rewardsForToken, token, user, address + " rewardsForToken");
   }
 
   public async getName(address: string): Promise<string> {
-    return this.tetuApi.web3Call0(this.instance(address).name, address + ' name');
+    return this.tetuApi.web3Call0(this.instance(address).name, address + " name");
   }
 
   public async getSymbol(address: string): Promise<string> {
-    return this.tetuApi.web3Call0(this.instance(address).symbol, address + ' symbol');
+    return this.tetuApi.web3Call0(this.instance(address).symbol, address + " symbol");
   }
 
 }
