18f313c9dacdc0db71cb6b0b7adfd4661055a902
Azarov Serge <s.a.azarov@gmail.com>
2021-09-07 01:58:07 +0300

    Add new tests utils

--- tests/test-utils.ts
+++ tests/test-utils.ts
@@ -1,6 +1,27 @@
-import {BigNumber, utils} from "ethers";
-import {expect} from "chai";
+import { BigNumber, utils } from "ethers";
+import Web3 from "web3";
+import { expect } from "chai";
 
 export function assertBN(bn: BigNumber, expected: number, dec = 18): void {
   expect(+utils.formatUnits(bn, dec)).is.eq(expected);
 }
+
+export const isBigNumber = (value: any): boolean => value instanceof BigNumber;
+
+export const isBigNumbers = (values: any[]): boolean =>
+  values.every((value) => isBigNumber(value));
+
+export const isAddresses = (values: any[]): boolean =>
+  values.every((value) => Web3.utils.isAddress(value));
+
+export const arrayHasArray = (values: any[]): boolean =>
+  values.some((value) => Array.isArray(value));
+
+export const arrayHasString = (values: any[]): boolean =>
+  values.some((value) => typeof value === "string");
+
+export const arrayHasAddress= (values: any[]): boolean =>
+  values.some((value) => Web3.utils.isAddress(value));
+
+export const arrayHasBigNumber = (values: any[]): boolean =>
+  values.some((value) => isBigNumber(value));
