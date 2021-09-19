3acf65b13a43b4196019b0385a6f76557d692e33
Azarov Serge <s.a.azarov@gmail.com>
2021-09-19 21:32:56 +0300

    Add methods for VaultService

--- src/services/vault-service.ts
+++ src/services/vault-service.ts
@@ -18,6 +18,66 @@ export class VaultService {
     return SmartVault__factory.connect(address, this.provider);
   }
 
+  public async getVersion(address: string): Promise<string> {
+    return this.tetuApi.web3Call0(this.instance(address).VERSION, address + " VERSION");
+  }
+
+  public async isActive(address: string): Promise<boolean> {
+    return this.tetuApi.web3Call0(this.instance(address).active, address + " active");
+  }
+
+  public async getAllowance(address: string, ownerAddress: string, spenderAddress: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call2(this.instance(address).allowance, ownerAddress,  spenderAddress, address + " allowance");
+  }
+
+  public async getAvailableToInvestOut(address: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call0(this.instance(address).availableToInvestOut, address + " availableToInvestOut");
+  }
+
+  public async getBalanceOf(address: string, token: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call1(this.instance(address).balanceOf, token, address + " balanceOf");
+  }
+
+  public async getController(address: string): Promise<string> {
+    return this.tetuApi.web3Call0(this.instance(address).controller, address + " controller");
+  }
+
+  public async getCreated(address: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call0(this.instance(address).created, address + " created");
+  }
+
+  public async getDecimals(address: string): Promise<number> {
+    return this.tetuApi.web3Call0(this.instance(address).decimals, address + " decimals");
+  }
+
+  public async getEarned(address: string, rtAddress: string, accountAddress: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call2(this.instance(address).earned, rtAddress,  accountAddress, address + " earned");
+  }
+
+  public async getEarnedWithBoost(address: string, rtAddress: string, accountAddress: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call2(this.instance(address).earnedWithBoost, rtAddress,  accountAddress, address + " earnedWithBoost");
+  }
+
+  public async getPricePerFullShare(address: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call0(this.instance(address).getPricePerFullShare, address + " getPricePerFullShare");
+  }
+
+  public async getRewardTokenIndex(address: string, token: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call1(this.instance(address).getRewardTokenIndex, token, address + " getRewardTokenIndex");
+  }
+
+  public async isController(address: string, token: string): Promise<boolean> {
+    return this.tetuApi.web3Call1(this.instance(address).isController, token, address + " isController");
+  }
+
+  public async isGovernance(address: string, token: string): Promise<boolean> {
+    return this.tetuApi.web3Call1(this.instance(address).isGovernance, token, address + " isGovernance");
+  }
+
+  public async getLastTimeRewardApplicable(address: string, token: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call1(this.instance(address).lastTimeRewardApplicable, token, address + " lastTimeRewardApplicable");
+  }
+
   public async getPeriodFinishForToken(address: string, token: string): Promise<BigNumber> {
     return this.tetuApi.web3Call1(this.instance(address).periodFinishForToken, token, address + " periodFinishForToken");
   }
@@ -48,6 +108,46 @@ export class VaultService {
 
   public async getSymbol(address: string): Promise<string> {
     return this.tetuApi.web3Call0(this.instance(address).symbol, address + " symbol");
+  }
+
+  public async getRewardTokens(address: string): Promise<string[]> {
+    return this.tetuApi.web3Call0(this.instance(address).rewardTokens, address + " rewardTokens");
+  }
+
+  public async getRewardTokensLength(address: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call0(this.instance(address).rewardTokensLength, address + " rewardTokensLength");
+  }
+
+  public async getStrategy(address: string): Promise<string> {
+    return this.tetuApi.web3Call0(this.instance(address).strategy, address + " strategy");
+  }
+
+  public async getTotalSupply(address: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call0(this.instance(address).totalSupply, address + " totalSupply");
+  }
+
+  public async getUnderlying(address: string): Promise<string> {
+    return this.tetuApi.web3Call0(this.instance(address).underlying, address + " underlying");
+  }
+
+  public async getUnderlyingBalanceInVault(address: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call0(this.instance(address).underlyingBalanceInVault, address + " underlyingBalanceInVault");
+  }
+
+  public async getUnderlyingBalanceWithInvestment(address: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call0(this.instance(address).underlyingBalanceWithInvestment, address + " underlyingBalanceWithInvestment");
+  }
+
+  public async getUnderlyingBalanceWithInvestmentForHolder(address: string, token: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call1(this.instance(address).underlyingBalanceWithInvestmentForHolder, token, address + " underlyingBalanceWithInvestmentForHolder");
+  }
+
+  public async getUnderlyingUnit(address: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call0(this.instance(address).underlyingUnit, address + " underlyingUnit");
+  }
+
+  public async getUserBoostTs(address: string, token: string): Promise<BigNumber> {
+    return this.tetuApi.web3Call1(this.instance(address).userBoostTs, token, address + " userBoostTs");
   }
 
 }
--- tests/services/vault-service.test.ts
+++ tests/services/vault-service.test.ts
@@ -2,7 +2,7 @@ import Web3 from "web3";
 import chai, {expect} from "chai";
 import chaiAsPromised from "chai-as-promised";
 import {Addresses} from "../../src/addresses";
-import {assertBN} from "../test-utils";
+import {assertBN, isBigNumber} from "../test-utils";
 import {TetuApi} from "../../src/tetu-api";
 import * as dotenv from "dotenv";
 
@@ -22,6 +22,114 @@ describe("Vault service tests", (): void => {
     tetuApi = new TetuApi(web3, 137);
   });
 
+  it("VERSION should be correct", async () => {
+    const result = await tetuApi.vaultService.getVersion("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(typeof result === "string")
+    .is.equal(true);
+  });
+
+  it("active should be correct", async () => {
+    const result = await tetuApi.vaultService.isActive("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(typeof result === "boolean")
+    .is.equal(true);
+  });
+
+  it("allowance should be correct", async () => {
+    const result = await tetuApi.vaultService.getAllowance("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454", "0xA56522BCA0A09f57B85C52c0Cc8Ba1B5eDbc64ef", "0x269A36957Bc7aDDee51c24AAC6f07c40dDFaCBC3");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+  it("availableToInvestOut should be correct", async () => {
+    const result = await tetuApi.vaultService.getAvailableToInvestOut("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+  it("balanceOf should be correct", async () => {
+    const result = await tetuApi.vaultService.getBalanceOf("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454", "0xA56522BCA0A09f57B85C52c0Cc8Ba1B5eDbc64ef");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+  it("controller should be correct", async () => {
+    const result = await tetuApi.vaultService.getController("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(Web3.utils.isAddress(result))
+    .is.equal(true);
+  });
+
+  it("created should be correct", async () => {
+    const result = await tetuApi.vaultService.getCreated("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+  it("decimals should be correct", async () => {
+    const result = await tetuApi.vaultService.getDecimals("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(typeof result === "number")
+    .is.equal(true);
+
+    expect(result)
+    .is.equal(18);
+  });
+
+  it("earned should be correct", async () => {
+    const result = await tetuApi.vaultService.getEarned("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454", "0xA56522BCA0A09f57B85C52c0Cc8Ba1B5eDbc64ef", "0x269A36957Bc7aDDee51c24AAC6f07c40dDFaCBC3");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+  it("earnedWithBoost should be correct", async () => {
+    const result = await tetuApi.vaultService.getEarnedWithBoost("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454", "0xA56522BCA0A09f57B85C52c0Cc8Ba1B5eDbc64ef", "0x269A36957Bc7aDDee51c24AAC6f07c40dDFaCBC3");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+
+  it("getPricePerFullShare should be correct", async () => {
+    const result = await tetuApi.vaultService.getPricePerFullShare("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+  it("getRewardTokenIndex should be correct", async () => {
+    const result = await tetuApi.vaultService.getRewardTokenIndex("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454", "0xA56522BCA0A09f57B85C52c0Cc8Ba1B5eDbc64ef");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+  it("isController should be correct", async () => {
+    const result = await tetuApi.vaultService.isController("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454", "0xA56522BCA0A09f57B85C52c0Cc8Ba1B5eDbc64ef");
+
+    expect(typeof result === "boolean")
+    .is.equal(true);
+  });
+
+  it("isGovernance should be correct", async () => {
+    const result = await tetuApi.vaultService.isGovernance("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454", "0xA56522BCA0A09f57B85C52c0Cc8Ba1B5eDbc64ef");
+
+    expect(typeof result === "boolean")
+    .is.equal(true);
+  });
+
+  it("lastTimeRewardApplicable should be correct", async () => {
+    const result = await tetuApi.vaultService.getLastTimeRewardApplicable("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454", "0xA56522BCA0A09f57B85C52c0Cc8Ba1B5eDbc64ef");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
 
   it("name should be correct", async () => {
     expect(await tetuApi.vaultService.getName("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454"))
@@ -68,4 +176,75 @@ describe("Vault service tests", (): void => {
         Addresses.ZERO_ADDRESS
     ), 0);
   });
+
+  it("rewardTokens should be correct", async () => {
+    const result = await tetuApi.vaultService.getRewardTokens("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(Array.isArray(result))
+    .is.equal(true);
+  });
+
+  it("rewardTokensLength should be correct", async () => {
+    const result = await tetuApi.vaultService.getRewardTokensLength("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+  it("strategy should be correct", async () => {
+    const result = await tetuApi.vaultService.getStrategy("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(Web3.utils.isAddress(result))
+    .is.equal(true);
+  });
+
+  it("totalSupply should be correct", async () => {
+    const result = await tetuApi.vaultService.getTotalSupply("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+  it("underlying should be correct", async () => {
+    const result = await tetuApi.vaultService.getUnderlying("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(Web3.utils.isAddress(result))
+    .is.equal(true);
+  });
+
+  it("underlyingBalanceInVault should be correct", async () => {
+    const result = await tetuApi.vaultService.getUnderlyingBalanceInVault("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+  it("underlyingBalanceWithInvestment should be correct", async () => {
+    const result = await tetuApi.vaultService.getUnderlyingBalanceWithInvestment("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+  it("underlyingBalanceWithInvestmentForHolder should be correct", async () => {
+    const result = await tetuApi.vaultService.getUnderlyingBalanceWithInvestmentForHolder("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454", "0xA56522BCA0A09f57B85C52c0Cc8Ba1B5eDbc64ef");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+  it("underlyingUnit should be correct", async () => {
+    const result = await tetuApi.vaultService.getUnderlyingUnit("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
+  it("userBoostTs should be correct", async () => {
+    const result = await tetuApi.vaultService.getUserBoostTs("0x225084D30cc297F3b177d9f93f5C3Ab8fb6a1454", "0xA56522BCA0A09f57B85C52c0Cc8Ba1B5eDbc64ef");
+
+    expect(isBigNumber(result))
+    .is.equal(true);
+  });
+
 });
